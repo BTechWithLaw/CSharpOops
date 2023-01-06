9150efc0c90e12cd7c4a26e531bc0a869fac312f
BTechWithLaw <BTechWithLaw@gmail.com>
2023-01-06 11:53:29 +0530

    Types of Constructor  1. Default Constructor 2. Parameterized Constructor  3. Copy Constructor  4. Private Constructor  5. Static Constructor

---
+++ ClassLibrary1/ConsoleApp1/ConstructorsDemoClass.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class ConstructorsDemoClass
+    {
+        private string month;
+        private int year;
+
+        //Default Constructor
+        public ConstructorsDemoClass()
+        {
+            Console.WriteLine("Default Constructor");
+        }
+        //------------------------------------------//
+
+
+        //Parameterized Constructor
+        public ConstructorsDemoClass(string input)
+        {
+            Console.WriteLine("Parameterized Constructor");
+        }
+        //------------------------------------------//
+
+
+
+        // declaring Copy constructor
+        public ConstructorsDemoClass(ConstructorsDemoClass s)
+        {
+            month = s.month;
+            year = s.year;
+        }
+
+        // Instance constructor
+        public ConstructorsDemoClass(string month, int year)
+        {
+            this.month = month;
+            this.year = year;
+        }
+
+        // Get details of ConstructorsDemoClass
+        public string Details
+        {
+            get
+            {
+                return "Month: " + month.ToString() +
+                         "\nYear: " + year.ToString();
+            }
+        }
+
+      
+        public void GetCopyConstructorDetails()
+        {
+            // Create a new ConstructorsDemoClass object.
+            ConstructorsDemoClass g1 = new ConstructorsDemoClass("January", 2023);
+
+            // here is g1 details is copied to g2.
+            ConstructorsDemoClass g2 = new ConstructorsDemoClass(g1);
+
+            Console.WriteLine("Copy Constructor: "+g2.Details);
+        }
+        //------------------------------------------//
+
+
+        static ConstructorsDemoClass()
+        {
+            Console.WriteLine("Static Constructor");
+            Console.WriteLine(@"
+1. Static Constructor has to be invoked only once in the class 
+2. And it has been invoked during the creation of the first reference to a static member in the class.
+3. A static constructor is initialized static fields or data of the class and to be executed only once.
+4. We don't have control over static constructor it will aoutomatically invoke first time and does not invoke again.
+");
+        }
+        //------------------------------------------//
+
+        //CONFUSE
+        //private ConstructorsDemoClass(int id)
+        //{
+        //    Console.WriteLine("Private Constructor");
+        //}
+
+
+    }
+}
--- ClassLibrary1/ConsoleApp1/Program.cs
+++ ClassLibrary1/ConsoleApp1/Program.cs
@@ -15,6 +15,41 @@ namespace ConsoleApp1
             Class1 c1 = new Class1();
             Console.WriteLine(c1.RemoveSpecialCharactersFromString("BVK!@#$%^&*()"));
             Console.ReadKey();
+
+            Console.WriteLine("Constructor Demo");
+            Console.WriteLine("1. Constructor of a class must have the same name as the class name in which it resides.");
+            Console.WriteLine("2. A constructor can not be abstract, final, and Synchronized.");
+            Console.WriteLine("3. Within a class, you can create only one static constructor.");
+            Console.WriteLine("4. A constructor doesn’t have any return type, not even void.");
+            Console.WriteLine("5. A static constructor cannot be a parameterized constructor.");
+            Console.WriteLine("6. A class can have any number of constructors.");
+            Console.WriteLine("7. ccess modifiers can be used in constructor declaration to control its access i.e which other class can call the constructor.");
+            Console.WriteLine(@"Types of Constructor 
+                                1. Default Constructor 
+                                2. Parameterized Constructor 
+                                3. Copy Constructor 
+                                4. Private Constructor 
+                                5. Static Constructor ");
+            Console.WriteLine();
+
+
+            ConstructorsDemoClass cd = new ConstructorsDemoClass();
+            Console.WriteLine("---------------------");
+
+            ConstructorsDemoClass cdc = new ConstructorsDemoClass("Parameterized Constructor");
+            Console.WriteLine("---------------------");
+
+
+            // Create a new ConstructorsDemoClass object.
+            ConstructorsDemoClass g1 = new ConstructorsDemoClass("January", 2023);
+
+            // here is g1 details is copied to g2.
+            ConstructorsDemoClass g2 = new ConstructorsDemoClass(g1);
+
+            Console.WriteLine("Copy Constructor with value equals to : " + g2.Details);
+
+            Console.ReadKey();
+
         }
     }
 }
